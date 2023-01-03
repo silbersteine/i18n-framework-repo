cda1c1d347c55222538c22dcc37830c57ab0a0eb
silbersteine13 <108279588+silbersteine13@users.noreply.github.com>
2023-01-03 11:16:02 -0600

    Configure lazy loading and create locale files

--- src/i18n.js
+++ src/i18n.js
@@ -1,25 +1,13 @@
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
 
 i18next
     .use(initReactI18next)
     .use(LanguageDetector)
+    .use(Backend)
         .init({
             debug: true,
-            fallbackLng: 'en',
-            resources: {
-                en: {
-                    translation: {
-                        learnReact: 'Learn React',
-                        description: ' Edit <1>src/App.js</1> and save to reload.'
-                    }
-                },
-                de: {
-                    translation: {
-                        learnReact: 'Lerne React',
-                        description: 'Editiere <1>src/App.js</1> und speichere um neu zu laden.'
-                    }
-                }
-            }
+            fallbackLng: 'en'
         })
--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import './i18n'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 );
 
