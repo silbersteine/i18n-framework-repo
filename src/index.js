e7a172767bc04a471c821ee2ab1c08ba0d552f8c
silbersteine13 <eric.silberstein@lokalise.com>
2023-01-04 23:01:32 +0000

    Fixed configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// I removed the /client from react-dom. Having done this the browser can now display my app.
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { IntlProvider } from 'react-intl';
-import Spanish from "./locales/es/translations.json";
-import English from "./locales/en/translations.json";
+import Spanish from "./lang/es.json";
+import English from "./lang/en.json";
 
 // get user's current local
 const local = navigator.language;
@@ -17,4 +18,8 @@ if (local === "en-US") {
 }
 
 // Local injects formatting specific to local of user. Language specifies messages translation
-ReactDOM.render(<IntlProvider locale={local} messages={lang}><App /></IntlProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <IntlProvider locale={local} messages={lang}>
+        <App />
+    </IntlProvider>, 
+    document.getElementById('root'));
