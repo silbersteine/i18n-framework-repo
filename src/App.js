fcfd0dcf2e394ffea5f1372e101d69078f694fb2
silbersteine13 <eric.silberstein@lokalise.com>
2023-01-05 01:01:15 +0000

    Add language switcher

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { FormattedMessage, FormattedDate } from "react-intl";
+import { Context } from "./components/Wrapper"
 
 function App(props) {
+  const context = useContext(Context);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* Language Switcher */}
+        <select value={context.locale} onChange={context.selectLang}>
+          <option value="en-US">English</option>
+          <option value="es-MX">Español</option>
+        </select>
+
         <p>
           <FormattedMessage
             id="editAndSave"
             defaultMessage= "Edit <code>{fileName}</code> and save to reload."
             values = {
               // code value must match the html attribute in the text
-              {fileName: "src/app.js", code: (word) => <code>{word}</code>}
+              {fileName: "src/app.js", code: (word) => <code>{word}</code>} 
             }
           />
         </p>
