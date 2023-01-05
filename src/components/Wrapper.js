e1642ced314919aa46120f5399276c38b521569e
silbersteine13 <eric.silberstein@lokalise.com>
2023-01-05 00:48:47 +0000

    Add Wrapper component for locale selector

---
+++ src/components/Wrapper.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { IntlProvider } from "react-intl";
+import Spanish from "../lang/es.json";
+import English from "../lang/en.json";
+
+const Context = React.createContext();
+
+// get user's current local
+const local = navigator.language;
+
+let lang;
+if (local === "en-US") {
+    lang = English;
+} else {
+    lang = Spanish;
+}
+    
+const Wrapper = (props) => {
+    const [locale, setLocale] = useState(local);
+    const [messages, setMessages] = useState(lang);
+
+    function selectLang(e) {
+        const newLocale = e.target.value;
+        setLocale(newLocale);
+        if (newLocale === "es-MX") {
+            setMessages(Spanish);
+        } else {
+            setMessages(English)
+        }
+    }
+
+    return (
+        <Context.Provider value = {{locale, selectLang}}>
+            <IntlProvider messages={messages} locale={locale}>
+                {props.children}
+            </IntlProvider>
+        </Context.Provider>
+    );
+};
+
+export default Wrapper;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { IntlProvider } from 'react-intl';
-import Spanish from "./lang/es.json";
-import English from "./lang/en.json";
+import Wrapper from "./components/Wrapper";
 
-// get user's current local
-const local = navigator.language;
 
-let lang;
-if (local === "en-US") {
-    lang = English;
-} else {
-    lang = Spanish;
-}
-
-// Local injects formatting specific to local of user. Language specifies messages translation
-//Change local to es-MX to see formatting of date changed to Mexican Spanish.
 ReactDOM.render(
-    <IntlProvider locale={local} messages={English} date={Date.now()}>
-        <App />
-    </IntlProvider>, 
+    <Wrapper>
+        <App date={Date.now()} />
+    </Wrapper>,
     document.getElementById('root'));
