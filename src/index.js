217f2974f3de46123b7a8be3e79751a08c64bda2
silbersteine13 <108279588+silbersteine13@users.noreply.github.com>
2023-01-24 14:53:27 -0600

    setup index file with dependencies and wrapper, remove comment from .linguirc, run compile

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { messages } from './locales/en/messages'
+
+i18n.load('en', messages)
+i18n.activate('en')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <I18nProvider i18n={i18n}>
     <App />
-  </React.StrictMode>
+  </I18nProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
